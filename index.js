835b1c0adfdb3f56e341dcc9d37caee4d0845653
Darryl Brown <darrylbrown15@gmail.com>
2019-11-23 21:59:37 +0000

    auto Format

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-var myNames = ["mike", "june", "danny", "mike"];
-myNames[0] = "rico";
-myNameAges = [23, 43, 24, 53];
-console.log("You are " + myNames[0] + " " + myNameAges[0]);
-
-//Create a multiple-deminsional array
 myArray = [[0, 9, 3],[32, 12, 43], 78, 53];
 //pop method is use to get last or end value.
 let thisPop = myArray.pop();
